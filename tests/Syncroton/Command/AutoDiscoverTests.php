cc70594e3720832a90b4d838bc59da76a1928cc0
Lars Kneschke <l.kneschke@metaways.de>
2013-10-17 12:54:08 +0200

    Implemented basic support for Autodiscover command
    Change-Id: I57c1bffa249608578129e7680a0cfb816e0b2343

---
+++ tests/Syncroton/Command/AutoDiscoverTests.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Syncroton
+ *
+ * @package     Syncroton
+ * @subpackage  Tests
+ * @license     http://www.tine20.org/licenses/lgpl.html LGPL Version 3
+ * @copyright   Copyright (c) 2013-2013 Metaways Infosystems GmbH (http://www.metaways.de)
+ * @author      Lars Kneschke <l.kneschke@metaways.de>
+ */
+
+/**
+ * class to test Syncroton_Command_AutoDiscover
+ *
+ * @package     Syncroton
+ * @subpackage  Tests
+ */
+class Syncroton_Command_AutoDiscoverTests extends PHPUnit_Framework_TestCase
+{
+    /**
+     * test creation of claendar folder
+     */
+    public function testCreateCalendarFolder()
+    {
+        $doc = new DOMDocument();
+        $doc->loadXML('<?xml version="1.0" encoding="utf-8"?>
+            <Autodiscover xmlns="http://schemas.microsoft.com/exchange/autodiscover/mobilesync/requestschema/2006">
+                <Request>
+                    <EMailAddress>chris@example.com</EMailAddress>
+                    <AcceptableResponseSchema>
+                        http://schemas.microsoft.com/exchange/autodiscover/mobilesync/responseschema/2006
+                    </AcceptableResponseSchema>
+                </Request>
+            </Autodiscover>
+        ');
+        
+        $autoDiscover = new Syncroton_Command_AutoDiscover($doc);
+        $autoDiscover->mobileSyncUrl = 'https://sync.example.com/Microsoft-Server-ActiveSync';
+        $autoDiscover->certEnrollUrl = 'https://sync.example.com/CertEnroll';
+        
+        $autoDiscover->handle();
+        
+        $responseDoc = $autoDiscover->getResponse();
+        #$responseDoc->formatOutput = true; echo $responseDoc->saveXML();
+        
+        $xpath = new DomXPath($responseDoc);
+        $xpath->registerNamespace('2006', 'http://schemas.microsoft.com/exchange/autodiscover/mobilesync/requestschema/2006');
+        
+        $nodes = $xpath->query('//2006:Autodiscover/Response/User');
+        $this->assertEquals(1, $nodes->length, $responseDoc->saveXML());
+        
+        $nodes = $xpath->query('//2006:Autodiscover/Response/Action');
+        $this->assertEquals(1, $nodes->length, $responseDoc->saveXML());
+        
+        $nodes = $xpath->query('//2006:Autodiscover/Response/Action/Settings');
+        $this->assertEquals(1, $nodes->length, $responseDoc->saveXML());
+        
+        $nodes = $xpath->query('//2006:Autodiscover/Response/Action/Settings/Server');
+        $this->assertEquals(2, $nodes->length, $responseDoc->saveXML());
+    }
+}
